c941d574e0f6f0522c1f33dec5b892ffcb75ddf3
samiyordanovfr-lang <samiyordanovfr@gmail.com>
2026-03-16 21:42:24 +0200

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -1,21 +1,13 @@
-const ytdl = require('ytdl-core');
-
-export default async function handler(req, res) {
+export default function handler(req, res) {
   const { url } = req.query;
-
-  if (!url) {
-    return res.status(400).json({ error: 'Липсва URL' });
+  let videoId = "YouTube Video";
+  if (url && url.includes('v=')) {
+    videoId = url.split('v=')[1].split('&')[0];
   }
-
-  try {
-    const info = await ytdl.getInfo(url);
-    res.status(200).json({
-      success: true,
-      title: info.videoDetails.title,
-      duration: Math.floor(info.videoDetails.lengthSeconds / 60) + " мин.",
-      viral_score: Math.floor(Math.random() * 20) + 80 + "%" // Имитираме висок скор
-    });
-  } catch (error) {
-    res.status(500).json({ error: 'Грешка при четене на видеото' });
-  }
+  res.status(200).json({
+    success: true,
+    title: "Клип: " + videoId,
+    duration: "Готов за обработка",
+    viral_score: Math.floor(Math.random() * 10) + 85 + "%"
+  });
 }
